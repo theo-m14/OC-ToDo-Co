bd9d783ebb1b23762989b6cacf1a54835f697f78
Michel Theo <theo.michel2@outlook.fr>
2024-02-20 16:50:53 +0100

    writing test and docker configuration

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexWithoutLogin()
     {
         $client = static::createClient();
 
+        $client->followRedirects();
         $crawler = $client->request('GET', '/');
 
+        $this->assertSame(1, $crawler->filter('input[name="_username"]')->count());
+        $this->assertSame(1, $crawler->filter('input[name="_password"]')->count());
+        $this->assertContains('/login',$crawler->getUri());
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
--- web/app.php
+++ web/app.php
@@ -2,6 +2,8 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+umask(0000);
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
