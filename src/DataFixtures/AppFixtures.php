358379bbe0534599cc9d635d945ae46a8b6f4ff9
Michel Théo <theo.michel2@outlook.fr>
2024-04-02 14:55:57 +0200

    run fixture in docker compose

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,8 +11,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
         TaskStory::load();
         $user = new User();
         $user->setEmail('theo@test.fr');
